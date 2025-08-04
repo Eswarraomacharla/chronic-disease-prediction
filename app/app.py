aa61e7ca089be7626f823ad4570f6cb400f72175
Abhayvk <eswarmarcharla143@gmail.com>
2025-08-04 16:26:00 +0530

    Fix path for Streamlit deployment

--- app/app.py
+++ app/app.py
@@ -3,7 +3,7 @@ import joblib
 from utils import preprocess_diabetes_input, preprocess_heart_input
 
 # Load models
-diabetes_model = joblib.load("D:/chronic disease prediction/chronic_disease_prediction/models/diabetes_model.pkl")
+diabetes_model = joblib.load("../models/diabetes_model.pkl")
 heart_model = joblib.load("D:/chronic disease prediction/chronic_disease_prediction/models/heart_model.pkl")
 
 # Sidebar for selection
